578a7107671b2fcc84cf174a600fda0ba1787dc8
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-11-11 20:15:08 +0100

    feat: added meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,25 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxt/icon",
     "nuxt-headlessui",
-    "nuxt-swiper"
+    "nuxt-swiper",
   ],
+  app: {
+    head: {
+      title: "Ozoz Sokoh",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Ozoz Sokoh is a food explorer and traveller by plate, educator and researcher exploring Nigeria and West African diaspora and Global Foodways. She believes that Food is More than Eating.",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+    },
+  },
 });
