e93435b3f684d10d63e9deb5c959d7851d303631
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-11-21 09:38:03 +0100

    feat: updated image width

--- components/Modal.vue
+++ components/Modal.vue
@@ -9,7 +9,7 @@ const clickedAway = (e: any) => {
 
 <template>
   <div class="bg-black/50 fixed top-0 z-[1000] w-full h-full flex items-center justify-center">
-    <img :src="`/img/${data.clickedImg}.webp`" alt="first large" class="w-11/12 lg:w-5/6" v-click-away="clickedAway">
+    <img :src="`/img/${data.clickedImg}.webp`" alt="first large" class="w-11/12 lg:w-7/12 2xl:w-5/6" v-click-away="clickedAway">
   </div>
 </template>
 
