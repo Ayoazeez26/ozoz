7dfe4a7431f5d22407c29c0baf405f25d32c4d6a
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2024-11-21 03:21:24 +0100

    feat: update

--- stores/data.ts
+++ stores/data.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useDataStore = defineStore("data", () => {
 
-  const clickedImg = ref("first");
+  const clickedImg = ref("");
   const showModal = ref(true);
 
   return {
